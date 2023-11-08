8131d3668a3b088df589e876968e284ade33b5c5
N0str4 <aymerickvalois@yahoo.fr>
2023-11-08 22:08:02 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
-  VITE_CORS_PROXY_URL: "",
-  VITE_TMDB_READ_API_KEY: ""
+  VITE_CORS_PROXY_URL: "https://basic-bundle-frosty-silence-bec3.aymerickvalois.workers.dev",
+  VITE_TMDB_READ_API_KEY: "e4860fa2d70600252f6dc5c39663498b"
 };
